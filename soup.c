1de2c2c61cc9758d20f8eddfa011b2f121d381aa
Jonathan Kang <jonathankang@gnome.org>
2019-01-18 17:26:42 +0800

    Quit the program while clicking the close button

--- soup.c
+++ soup.c
@@ -43,6 +43,7 @@ main (int argc, char *argv[])
     /* Create a GtkImage based on the pixbuf */
     image = gtk_image_new_from_pixbuf (pixbuf);
     window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+    g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
     gtk_container_add (GTK_CONTAINER (window), image);
     gtk_widget_show_all (window);
 
