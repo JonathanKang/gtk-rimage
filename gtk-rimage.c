f61827bb476a1ad5eb383a053da5d7fdffb6fb40
Jonathan Kang <jonathankang@gnome.org>
2019-01-26 18:50:00 +0800

    Use GdkPixbufLoader instead of GdkPixbuf

--- gtk-rimage.c
+++ gtk-rimage.c
@@ -3,12 +3,45 @@
 #include <gtk/gtk.h>
 #include <libsoup/soup.h>
 
+static void
+on_area_prepared (GdkPixbufLoader *loader,
+                  gpointer user_data)
+{
+    GdkPixbuf *pixbuf;
+    GtkWidget *image;
+
+    image = GTK_WIDGET (user_data);
+
+    pixbuf = gdk_pixbuf_loader_get_pixbuf (loader);
+    gdk_pixbuf_fill (pixbuf, 0xaaaaaaff);
+
+    gtk_image_set_from_pixbuf (GTK_IMAGE (image), pixbuf);
+}
+
+static void
+on_area_updated (GdkPixbufLoader *loader,
+                 gint             x,
+                 gint             y,
+                 gint             width,
+                 gint             height,
+                 gpointer         user_data)
+{
+    GdkPixbuf *pixbuf;
+    GtkWidget *image;
+
+    image = GTK_WIDGET (user_data);
+
+    pixbuf = gtk_image_get_pixbuf (GTK_IMAGE (image));
+    g_object_ref (pixbuf);
+    gtk_image_set_from_pixbuf (GTK_IMAGE (image), pixbuf);
+    g_object_unref (pixbuf);
+}
+
 int
 main (int argc, char *argv[])
 {
-    GdkPixbuf *pixbuf;
+    GdkPixbufLoader *loader;
     GError *error = NULL;
-    GInputStream *input_stream;
     GtkWidget *window;
     GtkWidget *image;
     guint status_code;
@@ -17,8 +50,15 @@ main (int argc, char *argv[])
 
     gtk_init (&argc, &argv);
 
+    image = gtk_image_new_from_pixbuf (NULL);
+    window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+    g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
+    gtk_container_add (GTK_CONTAINER (window), image);
+    gtk_widget_show_all (window);
+
     /* HTTP request */
     msg = soup_message_new ("GET", "https://avatars2.githubusercontent.com/u/5607743?s=460&v=4");
+    /* msg = soup_message_new (SOUP_METHOD_GET, "https://wx2.sinaimg.cn/mw690/6d3fe6d2gy1fzbzeojeh7j20u0182x6s.jpg"); */
     session = soup_session_new ();
     status_code = soup_session_send_message (session, msg);
     if (status_code != SOUP_STATUS_OK) {
@@ -27,10 +67,14 @@ main (int argc, char *argv[])
         return 1;
     }
 
-    input_stream = g_memory_input_stream_new_from_data (msg->response_body->data,
-                                                        msg->response_body->length,
-                                                        NULL);
-    pixbuf = gdk_pixbuf_new_from_stream (input_stream, NULL, &error);
+    loader = gdk_pixbuf_loader_new ();
+    g_signal_connect (loader, "area-prepared",
+                      G_CALLBACK (on_area_prepared), image);
+    g_signal_connect (loader, "area-updated",
+                      G_CALLBACK (on_area_updated), image);
+    gdk_pixbuf_loader_write (loader,
+                             (guchar *)msg->response_body->data,
+                             msg->response_body->length, &error);
     if (error != NULL)
     {
         g_warning ("unable to create pixbuf: %s",
@@ -40,24 +84,11 @@ main (int argc, char *argv[])
         return 1;
     }
 
-    /* Create a GtkImage based on the pixbuf */
-    image = gtk_image_new_from_pixbuf (pixbuf);
-    window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-    g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
-    gtk_container_add (GTK_CONTAINER (window), image);
-    gtk_widget_show_all (window);
-
     gtk_main ();
 
     g_object_unref (msg);
     g_object_unref (session);
-    g_input_stream_close (input_stream, NULL, &error);
-    if (error != NULL)
-    {
-        g_warning ("unable to close input stream: %s",
-                   error->message);
-        g_clear_error (&error);
-    }
+    gdk_pixbuf_loader_close (loader, &error);
 
     return 0;
 }
